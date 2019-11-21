1affe5b070c63418c38bf483e1faea2923f1863d
Student <example@example.com>
2019-11-20 16:37:30 -0800

    separated logic for omdb project

--- src/main.js
+++ src/main.js
@@ -3,28 +3,83 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { MovieService } from './../src/movies.js';
+import { GiphyService } from './../src/movies.js';
 
 
 $(document).ready(function() {
 
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+  $('#movieButton').click(function() {
+    const movie = $('#location').val();
     $('#location').val("");
 
     (async () => {
       let movieService = new MovieService();
-      const response = await movieService.getWeatherByCity(city);
+      const response = await movieService.getMovie(movie);
       getElements(response);
     })();
 
     function getElements(response) {
-      $('.showHumidity').text(`${response.Title}`);
-      $('.showTemp').text(`${response.Director}`);
+      $('.showMovie').text(`${response.Title}`);
+      $('.showDirector').text(`${response.Director}`);
     }
 
     setInterval (function() {
       $("#imageToPlay").toggle();
-    },1000);
+    },5000);
 
+
+    (async () => {
+      let giphyService = new GiphyService();
+      const response = await giphyService.getGif(movie);
+      getElements(response);
+    })();
+
+    // trying to plumb the front end of the giphy api
+
+    // $("#imageToToggle").attr("src", response.data.images.original.url);
+
+    // getAPI(movie);
+    //   getElements = function(response) {
+    //   $("#imageToToggle").attr("src", response.data.images.original.url);
+    // }};
   });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${searchTermWinLoss}`
+
+// const getElements = function(response) {
+//     $("#imageWinLoss").attr("src", response.data.images.original.url);
+//   };
+//
+// let searchTerm = "city";
+//
+//   let api = function() {
+//     let request = new XMLHttpRequest();
+//     const url = `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${searchTerm}`;
+//
+//     request.onreadystatechange = function() {
+//       if (this.readyState === 4 && this.status === 200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     };
+//
+//     request.open("GET", url, true);
+//     request.send();
+//
+//     const getElements = function(response) {
+//       $("#testGif").attr("src", response.data.images.original.url);
+//     };
+//   };
--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,8 @@
 
-
 export class MovieService {
-  async getWeatherByCity(city) {
+  async getMovie(movie) {
     try {
-      let response = await fetch(`http://www.omdbapi.com/?t=${city}&apikey=${process.env.API_KEY}`);
+      let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
@@ -11,3 +10,41 @@ export class MovieService {
     }
   }
 }
+  // trying to plumb the front end of the giphy api
+export class GiphyService {
+  async getGif(movie) {
+    try {
+      let response = await fetch(`http://api.giphy.com/v1/gifs/translate?q=${movie}&api_key=${process.env.API_KEY_GIPHY}&limit=1"`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
+    }
+  }
+}
+
+
+
+
+// export class GiphyService {
+//   getAPI(movie) {fetch(`http://api.giphy.com/v1/gifs/translate?q=${movie}&api_key=${process.env.API_KEY_GIPHY}&limit=1"`);
+//   console.log(movie);
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(jsonifiedResponse) {
+//     return getElements(jsonifiedResponse);
+//   });
+// }
+//
+//
+// export class GiphyService {
+//   getAPI(movie) {fetch(`http://api.giphy.com/v1/gifs/translate?q=${movie}&api_key=${process.env.API_KEY_GIPHY}&limit=1"`);
+//   console.log(movie);
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(jsonifiedResponse) {
+//     return getElements(jsonifiedResponse);
+//   });
+// }
